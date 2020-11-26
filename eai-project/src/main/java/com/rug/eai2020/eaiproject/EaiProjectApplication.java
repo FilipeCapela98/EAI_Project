9f5230443d35379dbd4ac8c42de71be62715e0f9
FilipeCapela98 <filipecapelafaculdade@gmail.com>
2020-11-26 13:04:27 +0100

    Initial code commit

---
+++ eai-project/src/main/java/com/rug/eai2020/eaiproject/EaiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rug.eai2020.eaiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaiProjectApplication.class, args);
+	}
+
+}
---
+++ eai-project/src/test/java/com/rug/eai2020/eaiproject/EaiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rug.eai2020.eaiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
