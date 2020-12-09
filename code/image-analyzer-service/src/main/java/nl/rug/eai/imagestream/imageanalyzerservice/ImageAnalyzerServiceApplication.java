53384e470f4c17a374597d4f84299092d4a1ca96
anil-rug <a.palayiparambil.mathew@student.rug.nl>
2020-12-09 01:32:36 +0100

    Cloud Vision API

--- code/image-analyzer-service/src/main/java/nl/rug/eai/imagestream/imageanalyzerservice/ImageAnalyzerServiceApplication.java
+++ code/image-analyzer-service/src/main/java/nl/rug/eai/imagestream/imageanalyzerservice/ImageAnalyzerServiceApplication.java
@@ -1,13 +1,18 @@
 package nl.rug.eai.imagestream.imageanalyzerservice;
 
+import nl.rug.eai.imagestream.imageanalyzerservice.service.CloudVisionService;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ImageAnalyzerServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImageAnalyzerServiceApplication.class, args);
+
+		ApplicationContext applicationContext = SpringApplication.run(ImageAnalyzerServiceApplication.class, args);
+		CloudVisionService visionService = applicationContext.getBean(CloudVisionService.class);
+		visionService.start();
 	}
 
 }
