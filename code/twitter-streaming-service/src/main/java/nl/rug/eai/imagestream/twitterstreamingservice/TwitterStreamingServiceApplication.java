7cf5378833265451b79f5eeb3a9e2c474079beb5
Swastik <swastiK23>
2020-12-11 00:42:26 +0530

    Adding twitter worksheet code

--- code/twitter-streaming-service/src/main/java/nl/rug/eai/imagestream/twitterstreamingservice/TwitterStreamingServiceApplication.java
+++ code/twitter-streaming-service/src/main/java/nl/rug/eai/imagestream/twitterstreamingservice/TwitterStreamingServiceApplication.java
@@ -2,12 +2,27 @@ package nl.rug.eai.imagestream.twitterstreamingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+import java.util.List;
 
 @SpringBootApplication
 public class TwitterStreamingServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TwitterStreamingServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterStreamingServiceApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(TwitterStreamingServiceApplication.class, args);
+        TwitterStreamTest twitterApp = applicationContext.getBean(TwitterStreamTest.class);
+        try {
+
+            List<String> Twitter = twitterApp.searchtweets();
+        } catch (TwitterException e) {
+            System.out.println("FAILED");
+        }
+
+        // twitter.start();
+    }
 
 }
