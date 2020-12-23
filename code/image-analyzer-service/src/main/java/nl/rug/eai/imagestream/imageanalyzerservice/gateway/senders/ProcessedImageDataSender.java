7cee49d5205f6eb59e2dfa91a8d3584aaa3cdc93
anil-rug <a.palayiparambil.mathew@student.rug.nl>
2020-12-23 01:15:23 +0100

    adding application config for JMS - image analysis

--- code/image-analyzer-service/src/main/java/nl/rug/eai/imagestream/imageanalyzerservice/gateway/senders/ProcessedImageDataSender.java
+++ code/image-analyzer-service/src/main/java/nl/rug/eai/imagestream/imageanalyzerservice/gateway/senders/ProcessedImageDataSender.java
@@ -1,18 +1,26 @@
 package nl.rug.eai.imagestream.imageanalyzerservice.gateway.senders;
 
+import lombok.extern.slf4j.Slf4j;
 import nl.rug.eai.imagestream.commons.model.AnnotatedImage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class ProcessedImageDataSender {
 
     @Autowired
     private JmsTemplate jmsTemplate;
 
     public void send(AnnotatedImage annotatedImage) {
-        jmsTemplate.convertAndSend("analyzed-images-stream", annotatedImage);
+        log.info("Sending data on queue: " + annotatedImage);
+        try {
+            jmsTemplate.convertAndSend("analyzed-images-stream", annotatedImage);
+        } catch (Exception e) {
+            log.error(e.toString());
+        }
+
     }
 
 }
